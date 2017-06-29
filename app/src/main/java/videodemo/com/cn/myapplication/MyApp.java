f7eeb0c7992f59c03f4d2b5eb01056ce202d4e36
yanjiangbo <bo@videopls.com>
2017-06-29 18:18:03 +0800

    升级版本到1.3.4.2

--- app/src/main/java/videodemo/com/cn/myapplication/MyApp.java
+++ app/src/main/java/videodemo/com/cn/myapplication/MyApp.java
@@ -2,10 +2,7 @@ package videodemo.com.cn.myapplication;
 
 import android.app.Application;
 
-import com.facebook.drawee.backends.pipeline.Fresco;
-import com.facebook.imagepipeline.core.ImagePipelineConfig;
-
-import cn.com.venvy.common.utils.VenvyDebug;
+import cn.com.venvy.VideoPlus;
 
 /**
  * Created by lgf on 2017/3/22.
@@ -16,11 +13,6 @@ public class MyApp extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-        ImagePipelineConfig config = ImagePipelineConfig.newBuilder(this).setDownsampleEnabled(true).build();
-        Fresco.initialize(this, config);
-        //强制设置为测试环境，如果要在正式环境测试，可删除下面代码
-        if (!VenvyDebug.getInstance().isDebug()) {
-            VenvyDebug.getInstance().toggle();
-        }
+        VideoPlus.appCreate(this);
     }
 }
